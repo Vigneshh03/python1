dd27defd712ff87ce220ef5699942bf4ac872e42
harshith <kanam.reddy@s.amity.edu>
2025-02-28 14:00:35 +0530

    added file a

---
+++ a.py
@@ -0,0 +1,11 @@
+n = 10
+a, b = 0, 1
+print("Fibonacci Series:", end=" ")
+for _ in range(n):
+    print(a, end=" ")
+    a, b = b, a + n = 10
+    a, b = 0, 1
+    print("Fibonacci Series:", end=" ")
+    for _ in range(n):
+            print(a, end=" ")
+                a, b = b, a + bb
